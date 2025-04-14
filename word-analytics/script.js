fe5587bfba869f37aaed9306f83b99b39e3e3f5d
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-15 02:24:38 +0300

    Implement word count calculation

--- word-analytics/script.js
+++ word-analytics/script.js
@@ -4,30 +4,39 @@ const twitterNumberElement = document.querySelector(".stat__number--twitter");
 const facebookNumberElement = document.querySelector(".stat__number--facebook");
 const wordsNumberElement = document.querySelector(".stat__number--words");
 
+function inputhandler() {   
 
+        // console.log(textareaElement.value.length);
+        //input validation
+        if (textareaElement.value.includes("  ")) {
+            alert("You can't use that.");
+            textareaElement.value = textareaElement.value.replace("  ", '');
+        }
+        
+        let numberofwords = textareaElement.value.split(' ').length;
+        if (textareaElement.value.length === 0) {
+            numberofwords = 0;
+        }
+        const numberofchars = textareaElement.value.length;
+        const twitterCharsLeft = 280 - numberofchars;
+        const facebookCharsLeft = 2200 - numberofchars;
+        
+        //for visual limit indicator
+        if (twitterCharsLeft < 0) {
+            twitterNumberElement.classList.add("stat__number--limit");
+        }else {
+            twitterNumberElement.classList.remove("stat__number--limit");
+        }
+        if (facebookCharsLeft < 0) {
+            facebookNumberElement.classList.add("stat__number--limit");
+        }else {
+            facebookNumberElement.classList.remove("stat__number--limit");
+        }
+        
+        charsNumberElement.textContent = numberofchars;
+        twitterNumberElement.textContent = twitterCharsLeft;
+        facebookNumberElement.textContent = facebookCharsLeft;
+        wordsNumberElement.textContent = numberofwords;
+        }
 
-textareaElement.addEventListener("input", () => {
-// console.log(textareaElement.value.length);
-
-const numberofchars = textareaElement.value.length;
-const twitterCharsLeft = 280 - numberofchars;
-const facebookCharsLeft = 2200 - numberofchars;
-
-//for visual limit indicator
-if (twitterCharsLeft < 0) {
-    textareaElement.classList.add("stat__number--limit");
-}else {
-    textareaElement.classList.remove("stat__number--limit");
-}
-if (facebookCharsLeft < 0) {
-    textareaElement.classList.add("stat__number--limit");
-}else {
-    textareaElement.classList.remove("stat__number--limit");
-}
-
-
-
-charsNumberElement.textContent = numberofchars;
-twitterNumberElement.textContent = twitterCharsLeft;
-facebookNumberElement.textContent = facebookCharsLeft;
-});
+        textareaElement.addEventListener("input", inputhandler);
